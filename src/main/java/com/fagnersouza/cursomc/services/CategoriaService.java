3d3b6d3593482cddddf7c786c6851f9ec3b0220d
Fagner Souza <fagner.roberto89@gmail.com>
2021-08-03 19:46:43 -0300

    Ajustes no endpoint /categorias/{id}

--- src/main/java/com/fagnersouza/cursomc/services/CategoriaService.java
+++ src/main/java/com/fagnersouza/cursomc/services/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.fagnersouza.cursomc.domain.Categoria;
 import com.fagnersouza.cursomc.repositories.CategoriaRepository;
+import com.fagnersouza.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -16,6 +17,7 @@ public class CategoriaService {
 
 	public Categoria find(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+		"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
+		}
 	}
-}
