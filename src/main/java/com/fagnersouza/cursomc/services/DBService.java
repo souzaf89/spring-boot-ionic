53ebd47d7e06752d4ec877698876d2cf63a39314
Fagner Souza <fagner.roberto89@gmail.com>
2021-08-22 19:03:23 -0300

    Adicionando senha a cliente

--- src/main/java/com/fagnersouza/cursomc/services/DBService.java
+++ src/main/java/com/fagnersouza/cursomc/services/DBService.java
@@ -5,6 +5,7 @@ import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.fagnersouza.cursomc.domain.Categoria;
@@ -32,6 +33,9 @@ import com.fagnersouza.cursomc.repositories.ProdutoRepository;
 
 @Service
 public class DBService {
+	
+	@Autowired
+	private BCryptPasswordEncoder pe;
 	
 	@Autowired
 	private CategoriaRepository categoriaRepository;
@@ -111,7 +115,7 @@ public class DBService {
 		estadoRepository.saveAll(Arrays.asList(est1, est2));
 		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
 		
-		Cliente cli1 = new Cliente(null, "Fagner Souza", "souza.fagner1989@gmail.com", "36378912377", TipoCliente.PESSOAFISICA);
+		Cliente cli1 = new Cliente(null, "Fagner Souza", "souza.fagner1989@gmail.com", "36378912377", TipoCliente.PESSOAFISICA, pe.encode("123"));
 		
 		cli1.getTelefones().addAll(Arrays.asList("27363323", "9383893"));
 		
