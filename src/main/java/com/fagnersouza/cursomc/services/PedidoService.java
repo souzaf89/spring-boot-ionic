d866d88b8c94eda1f3691520b52f9a225b1bd51e
Fagner Souza <fagner.roberto89@gmail.com>
2021-08-22 16:04:56 -0300

    Email HTML

--- src/main/java/com/fagnersouza/cursomc/services/PedidoService.java
+++ src/main/java/com/fagnersouza/cursomc/services/PedidoService.java
@@ -65,7 +65,7 @@ public class PedidoService {
 			ip.setPedido(obj);
 		}
 		itemPedidoRepository.saveAll(obj.getItens());
-		emailService.sendOrderConfirmationEmail(obj);
+		emailService.sendOrderConfirmationHtmlEmail(obj);
 		return obj;
 	}
 	
