0e787b0e7dfcfd516bda66f1732bee228d135cc0
Fagner Souza <fagner.roberto89@gmail.com>
2021-08-24 21:40:12 -0300

    Implementando autorizacao

--- src/main/java/com/fagnersouza/cursomc/config/SecurityConfig.java
+++ src/main/java/com/fagnersouza/cursomc/config/SecurityConfig.java
@@ -19,6 +19,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.fagnersouza.cursomc.security.JWTAuthenticationFilter;
+import com.fagnersouza.cursomc.security.JWTAuthorizationFilter;
 import com.fagnersouza.cursomc.security.JWTUtil;
 
 @Configuration
@@ -57,6 +58,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			.antMatchers(PUBLIC_MATCHERS).permitAll()
 			.anyRequest().authenticated();
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
 	
