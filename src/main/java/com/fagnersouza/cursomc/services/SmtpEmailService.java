cf2d9238992019846a0a5eb6bb1a53df91a8d6f9
Fagner Souza <fagner.roberto89@gmail.com>
2021-08-28 22:12:46 -0300

    Configuracao e envio do projeto para producao

--- src/main/java/com/fagnersouza/cursomc/services/SmtpEmailService.java
+++ src/main/java/com/fagnersouza/cursomc/services/SmtpEmailService.java
@@ -7,15 +7,14 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.MailSender;
 import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
 
 public class SmtpEmailService extends AbstractEmailService{
 
 	@Autowired
 	private MailSender mailSender;
 	
-	@Autowired
-	private JavaMailSender javaMailSender;
+	/*@Autowired
+	private JavaMailSender javaMailSender;*/
 	
 	private static final Logger LOG = LoggerFactory.getLogger(SmtpEmailService.class);
 	
@@ -28,10 +27,16 @@ public class SmtpEmailService extends AbstractEmailService{
 
 	@Override
 	public void sendHtmlEmail(MimeMessage msg) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	/*@Override
+	public void sendHtmlEmail(MimeMessage msg) {
 		LOG.info("Enviando email...");
 		javaMailSender.send(msg);
 		LOG.info("Email enviado");
 		
-	}
+	}*/
 
 }
