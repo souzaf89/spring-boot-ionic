7874a047739a15b2e2e43d90f1629d625d4d4b2b
Fagner Souza <fagner.roberto89@gmail.com>
2021-08-28 21:48:47 -0300

    Configuracao e envio do projeto para producao

--- src/main/java/com/fagnersouza/cursomc/CursomcApplication.java
+++ src/main/java/com/fagnersouza/cursomc/CursomcApplication.java
@@ -3,8 +3,7 @@ package com.fagnersouza.cursomc;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-@ComponentScan(basePackages = "com.fagnersouza.cursomc.services.EmailService")
+
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner{
 	
