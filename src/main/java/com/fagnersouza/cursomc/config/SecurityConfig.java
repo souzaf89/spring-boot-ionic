7248edb35d346b775cfd1984800eb630617e8779
Fagner Souza <fagner.roberto89@gmail.com>
2021-08-28 21:01:11 -0300

    Configuracao e envio do projeto para producao

--- src/main/java/com/fagnersouza/cursomc/config/SecurityConfig.java
+++ src/main/java/com/fagnersouza/cursomc/config/SecurityConfig.java
@@ -38,7 +38,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	private JWTUtil jwtUtil;
 	
 	private static final String[] PUBLIC_MATCHERS = {
-			"/h2-console/**"
+			"/h2-console/**",
+			"/app1-spring-ionic.herokuapp.com/**"
 	};
 
 	private static final String[] PUBLIC_MATCHERS_GET = {
