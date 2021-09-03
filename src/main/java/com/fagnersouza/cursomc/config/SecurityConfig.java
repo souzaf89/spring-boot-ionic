01d4b1ff4ca9e8e7cf1f93670fe39ae2213dd3c8
Fagner Souza <fagner.roberto89@gmail.com>
2021-09-02 23:13:18 -0300

    Enviando imagem via endpoint

--- src/main/java/com/fagnersouza/cursomc/config/SecurityConfig.java
+++ src/main/java/com/fagnersouza/cursomc/config/SecurityConfig.java
@@ -48,7 +48,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	};
 	
 	private static final String[] PUBLIC_MATCHERS_POST = {
-			"/clientes/**",
+			"/clientes",
+			"/clientes/picture",
 			"/auth/forgot/**"
 	};
 	
--- src/main/java/com/fagnersouza/cursomc/services/ClienteService.java
+++ src/main/java/com/fagnersouza/cursomc/services/ClienteService.java
@@ -1,5 +1,6 @@
 package com.fagnersouza.cursomc.services;
 
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
@@ -11,6 +12,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.fagnersouza.cursomc.domain.Cidade;
 import com.fagnersouza.cursomc.domain.Cliente;
@@ -41,6 +43,9 @@ public class ClienteService {
 	
 	@Autowired
 	private EnderecoRepository enderecoRepository;
+	
+	@Autowired
+	private S3Service s3Service;
 
 	public Cliente find(Integer id) {
 		
@@ -109,5 +114,9 @@ public class ClienteService {
 	private void updateData(Cliente newObj, Cliente obj) {
 		newObj.setNome(obj.getNome());
 		newObj.setEmail(obj.getEmail());
+	}
+	
+	public URI uploadProfilePicture(MultipartFile multipartFile) {
+		return s3Service.uploadFile(multipartFile);
 	}
 }
